a752f9e27c3b1b26d5eea460e810112ad970a50d
Mega1488 <korinto1488@gmail.com>
2017-09-08 23:35:24 -0300

    ws_estudios

--- GestionAcademica/src/java/WebService/ws_estudios.java
+++ GestionAcademica/src/java/WebService/ws_estudios.java
@@ -35,7 +35,6 @@ public class ws_estudios {
     @WebMethod(operationName = "lstEstudiosPorAlumno")
     public Retorno_MsgObj lstEstudiosPorAlumno(@WebParam(name = "token") String token, @WebParam(name = "PerCod") Long PerCod) 
     {
-        
         Retorno_MsgObj retorno = new Retorno_MsgObj();
         LoPersona lopersona = LoPersona.GetInstancia();
         ArrayList<SDT_PersonaEstudio> lstEstudios = new ArrayList<>();
@@ -55,7 +54,7 @@ public class ws_estudios {
                 lstEstudios = lopersona.ObtenerEstudios(PerCod);
                 for(SDT_PersonaEstudio lstEst : lstEstudios)
                 {
-                    retorno.setObjeto(lstEst);
+                    retorno.getLstObjetos().add(lstEst);
                 }
                 retorno.setMensaje(new Mensajes("OK", TipoMensaje.MENSAJE));
             }
@@ -86,91 +85,4 @@ public class ws_estudios {
         }
         return retorno;
     }
-    
-//    @WebMethod(operationName = "escolaridadAlumno")
-//    public Retorno_MsgObj escolaridadAlumno(@WebParam(name = "token") String token, @WebParam(name = "PerCod") Long PerCod) 
-//    {
-//        Retorno_MsgObj retorno = new Retorno_MsgObj();
-//        LoPersona loPersona = LoPersona.GetInstancia();
-//        ArrayList<SDT_PersonaEstudio> lstEstudio = new ArrayList<>();
-//        List<Object> lstEsc      = new ArrayList<>();
-//        Escolaridad esc;
-//        
-//        if(token == null)
-//        {
-//            retorno.setMensaje(new Mensajes("No se recibió ningún valor token", TipoMensaje.ERROR));
-//        }
-//        else
-//        {
-//            if(token == null)
-//            {
-//                retorno.setMensaje(new Mensajes("No se recibió ningún parametro Alumno", TipoMensaje.ERROR));
-//            }
-//            else
-//            { 
-//                lstEstudio  = loPersona.ObtenerEstudios(PerCod);
-//                for(SDT_PersonaEstudio est : lstEstudio)
-//                {
-//                    for (Escolaridad escolaridad : est.getEscolaridad())
-//                    {
-//                        esc  = new Escolaridad();
-//                        esc.setEscCod(escolaridad.getEscCod());
-//                        esc.setEscCalVal(escolaridad.getEscCalVal());
-//                        esc.setEscCurVal(escolaridad.getEscCurVal());
-//                        esc.setEscFch(escolaridad.getEscFch());
-//                        esc.setObjFchMod(escolaridad.getObjFchMod());
-//                        esc.setIngresadaPor(escolaridad.getIngresadaPor());
-//                        esc.setAlumno(escolaridad.getAlumno());
-//                        esc.setCurso(escolaridad.getCurso());
-//                        esc.setMateria(escolaridad.getMateria());
-//                        esc.setModulo(escolaridad.getModulo());
-//                        
-////                        lstEsc.add(esc);
-//                        retorno.setObjeto(esc);
-//                    }
-//                }
-//                retorno.setMensaje(new Mensajes("OK", TipoMensaje.MENSAJE));
-//            }
-//        }
-//        return retorno;
-//    }
-    
-//    @WebMethod(operationName = "documentoPorId")
-//    public Retorno_MsgObj documentoPorId(@WebParam(name = "token") String token, @WebParam(name = "PeriEstCod") Long PeriEstCod) 
-//    {
-//        Retorno_MsgObj retorno = new Retorno_MsgObj();
-//        LoPeriodo loPeriodo = LoPeriodo.GetInstancia();
-//        List<PeriodoEstudioDocumento> lstObjeto = new ArrayList<>();
-//        
-//        
-//        if(token == null)
-//        {
-//            retorno.setMensaje(new Mensajes("No se recibió ningún valor token", TipoMensaje.ERROR));
-//        }
-//        else
-//        {
-//            if(PeriEstCod == null)
-//            {
-//                retorno.setMensaje(new Mensajes("No se recibió ningún parametro Documento", TipoMensaje.ERROR));
-//            }
-//            else
-//            {
-//                retorno = (Retorno_MsgObj) loPeriodo.EstudioObtener(PeriEstCod);
-//                if (!retorno.SurgioErrorObjetoRequerido()) 
-//                {
-//                    lstObjeto = ((PeriodoEstudio) retorno.getObjeto()).getLstDocumento();
-//                    retorno.setObjeto(lstObjeto);
-//                }
-//            }
-//        }
-//        return retorno;
-//    }
-    
-//    @WebMethod(operationName = "stDocumentosById")
-//    public Retorno_MsgObj stDocumentosById(@WebParam(name = "token") String token, @WebParam(name = "UsuAlumno") Long UsuAlumno) 
-//    {
-//        Retorno_MsgObj retorno = new Retorno_MsgObj();
-//        
-//        return retorno;
-//    }
 }
